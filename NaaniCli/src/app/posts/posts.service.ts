ebfca703c7c149013eaa2003a6d358a66982cd3c
JPZ <jeanpaulzune@gmail.com>
2017-06-05 17:23:55 -0400

    Created post service and some configing.

---
+++ NaaniCli/src/app/posts/posts.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+
+import { Post } from './post';
+
+@Injectable()
+export class PostsService {
+
+  constructor() { }
+
+}
