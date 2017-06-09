5fbae04b355bc13a5652ea11767f8bbc6682da21
JPZ <jeanpaulzune@gmail.com>
2017-06-08 23:33:48 -0400

    Adjusted PostsService

--- NaaniCli/src/app/posts/posts.service.ts
+++ NaaniCli/src/app/posts/posts.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-
 import { Observable } from 'rxjs/Observable';
-
 import 'rxjs/add/operator/map';
-
 import { Post } from './post';
 
 @Injectable()
 export class PostsService {
 
-private postsUrl = "{YOUR-SITE-NAME}/wp-json/wp/v2/";
+private _wpBase = "{YOUR-SITE-NAME}/wp-json/wp/v2/";
 
   constructor(private http: Http) { }
 
   getPosts(): Observable<Post[]> {
 
     return this.http
-      .get(this.postsUrl + 'posts')
+      .get(this._wpBase + 'posts')
       .map((res: Response) => res.json());
   }
+
+  getPost(slug): Observable<Post> {
+
+      return this.http
+        .get(this._wpBase + `posts?slug=${slug}`)
+        .map((res: Response) => res.json());
+
+  }
+
 }
