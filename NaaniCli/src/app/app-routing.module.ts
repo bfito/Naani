4be88505ff961f26e7a88552894aee0584c441dc
JPZ <jeanpaulzune@gmail.com>
2017-06-08 23:24:13 -0400

    Routed Single Post

--- NaaniCli/src/app/app-routing.module.ts
+++ NaaniCli/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostSingleComponent } from './posts/post-single/post-single.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PostListComponent,
     pathMatch: 'full'
-  }
+  },
+  {
+  path: ':slug',
+  component: PostSingleComponent
+}
 ];
 
 @NgModule({
