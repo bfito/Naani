4c6d19d67bfa087b5d7f502d43d3e8fda6dc8c94
JPZ <jeanpaulzune@gmail.com>
2017-06-10 23:17:09 -0400

    Pulling the Slug From the Activated Route

--- NaaniCli/src/app/posts/post-single/post-single.component.ts
+++ NaaniCli/src/app/posts/post-single/post-single.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../post';
+import { PostsService } from '../posts.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-post-single',
   templateUrl: './post-single.component.html',
-  styleUrls: ['./post-single.component.css']
+  styleUrls: ['./post-single.component.css'],
+  providers: [PostsService]
 })
 export class PostSingleComponent implements OnInit {
 
